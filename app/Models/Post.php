d8c4375dfae06eed8e153b2f8b680c51dc03a5c4
Mu'adz Mohd Rosli <mu'adz.rosli@ionnex.com>
2022-10-17 11:20:20 +0800

    feat: create migrations and update community, post, comment and vote models

---
+++ app/Models/Post.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Post extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'user_id',
+        'community_id',
+        'title',
+        'slug',
+        'url',
+        'description',
+    ];
+
+    protected $casts = [
+        'user_id' => 'integer',
+        'community_id' => 'integer',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function community()
+    {
+        return $this->belongsTo(Community::class);
+    }
+
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -42,4 +42,24 @@ class User extends Authenticatable implements MustVerifyEmail
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function communities()
+    {
+        return $this->hasMany(Community::class);
+    }
+
+    public function posts()
+    {
+        return $this->hasMany(Post::class);
+    }
+
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
+
+    public function postVotes()
+    {
+        return $this->hasMany(PostVote::class);
+    }
 }
