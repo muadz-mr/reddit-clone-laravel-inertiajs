68bc91fda5a2937ec5e434a4b586f83d5ea758fd
Mu'adz Mohd Rosli <mu'adz.rosli@ionnex.com>
2022-10-17 11:24:46 +0800

    fix: add missiong relationships

--- app/Models/Community.php
+++ app/Models/Community.php
@@ -24,4 +24,9 @@ class Community extends Model
     {
         return $this->belongsTo(User::class);
     }
+
+    public function posts()
+    {
+        return $this->hasMany(Post::class);
+    }
 }
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -38,4 +38,9 @@ class Post extends Model
     {
         return $this->hasMany(Comment::class);
     }
+
+    public function postVotes()
+    {
+        return $this->hasMany(PostVote::class);
+    }
 }
